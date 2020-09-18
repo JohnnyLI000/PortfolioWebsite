5a79e3e71471622773898c16b308d721b6b9bf83
JohnnyLI000 <30562404+JohnnyLI000@users.noreply.github.com>
2020-09-18 20:07:03 +1200

    about me paragraph

--- portfolio/src/HomePage/Bottom.js
+++ portfolio/src/HomePage/Bottom.js
@@ -1,23 +1,25 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import emailjs from "emailjs-com"
 import './css/BottomStyle/Bottom.css'
 import LetsChat from "./LetsChat.js"
 class Bottom extends Component {
- 
-    
-    render() { 
-        return ( 
+
+
+    render() {
+        return (
             <React.Fragment>
                 <div className="background">
                     <div className="profile-image-container"></div>
                 </div>
-                <p>Hey again, my name is Johnny Li , a software developer, born in Chong Qing China . Currently based in Auckland. I am majoring in software engineering and game programming at Auckland University of Technology. I have done projects in multi-disciplines, including web app, software, game, mobile app, etc. But the journey of debugging and creativity does not end here…
-Also, a lover of photography, cooking, fitness, anime, and gaming of course. 
-</p>
+                <div className="about-me-container">
+                    Hey again, my name is Johnny Li , a software developer, born in Chong Qing China . Currently based in Auckland. I am majoring in software engineering and game programming at Auckland University of Technology. I have done projects in multi-disciplines, including web app, software, game, mobile app, etc. But the journey of debugging and creativity does not end here…
+                <br></br><br></br><br></br>Also, a lover of photography, cooking, fitness, anime, and gaming of course.
+                </div>
+
                 <LetsChat></LetsChat>
-        </React.Fragment>
+            </React.Fragment>
         );
     }
 }
- 
+
 export default Bottom;
